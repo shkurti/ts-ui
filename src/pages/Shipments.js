b7b03bee4534de64ae9c74c30a5e04ef6c4e2c7a
shkurti <andonshkurti@gmail.com>
2025-06-30 19:34:52 -0400

    added full url

--- src/pages/Shipments.js
+++ src/pages/Shipments.js
@@ -12,6 +12,8 @@ L.Icon.Default.mergeOptions({
   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
 });
 
+const BACKEND_URL = "https://backend-ts-68222fd8cfc0.herokuapp.com";
+
 const Shipments = () => {
   const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
   const [selectAll, setSelectAll] = useState(false);
@@ -60,7 +62,7 @@ const Shipments = () => {
     const fetchShipments = async () => {
       setIsLoading(true);
       try {
-        const response = await fetch('/shipment_meta');
+        const response = await fetch(`${BACKEND_URL}/shipment_meta`);
         if (response.ok) {
           const data = await response.json();
           console.log('Fetched shipments:', data); // Debug log
@@ -77,7 +79,7 @@ const Shipments = () => {
 
     const fetchTrackers = async () => {
       try {
-        const response = await fetch('/registered_trackers');
+        const response = await fetch(`${BACKEND_URL}/registered_trackers`);
         if (response.ok) {
           const data = await response.json();
           setTrackers(data);
@@ -126,7 +128,7 @@ const Shipments = () => {
     if (selectedShipments.length > 0) {
       try {
         const deletePromises = selectedShipments.map(shipmentId =>
-          fetch(`/shipment_meta/${shipmentId}`, {
+          fetch(`${BACKEND_URL}/shipment_meta/${shipmentId}`, {
             method: 'DELETE'
           })
         );
@@ -228,7 +230,7 @@ const Shipments = () => {
         }))
       };
 
-      const response = await fetch('/shipment_meta', {
+      const response = await fetch(`${BACKEND_URL}/shipment_meta`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(shipmentData),
@@ -240,7 +242,7 @@ const Shipments = () => {
         
         // Refetch all shipments from the database to get the correct data structure
         try {
-          const fetchResponse = await fetch('/shipment_meta');
+          const fetchResponse = await fetch(`${BACKEND_URL}/shipment_meta`);
           if (fetchResponse.ok) {
             const updatedShipments = await fetchResponse.json();
             setShipments(updatedShipments);
@@ -321,8 +323,7 @@ const Shipments = () => {
       });
       console.log('Sensor data fetch params:', params.toString());
       
-      
-      const response = await fetch(`/shipment_route_data?${params}`);
+      const response = await fetch(`${BACKEND_URL}/shipment_route_data?${params}`);
       if (response.ok) {
         const data = await response.json();
         console.log('Sensor data fetch params:', params.toString());
